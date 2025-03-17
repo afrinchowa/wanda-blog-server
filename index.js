e8d5d21c5dd2b58d630e1f58a87648b092185522
chowa <arfinchowa524@gmail.com>
2025-03-18 00:21:23 +0600

    sign up page work done by arif

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-const express = require('express');
-const app =express();
-const cors=require('cors');
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8dojk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,7 +24,19 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+    const database = client.db("wandaBlog");
+    const blogCollection = database.collection("blog");
+    const userCollection = client.db("wandaBlog").collection("users");
+
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+
+      console.log("creating new user", newUser);
+      const result = await userCollection.insertOne(newUser)
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -36,12 +44,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-// wandaBlogUser
-// N3oUK3GP5umbnhY9
-app.get('/',(req,res)=>{
-    res.send('wanda is running')
-})
+app.get("/", (req, res) => {
+  res.send("wanda is running");
+});
 
-app.listen(port,()=>{
-    console.log(`Bistro boss is sitting on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Bistro boss is sitting on port ${port}`);
+});
